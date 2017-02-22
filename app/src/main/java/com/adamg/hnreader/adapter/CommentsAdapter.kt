c4c022195412b89b75515d312f01569c65ab1223
AdamGrzybkowski <agrzybkowski@outlook.com>
2017-02-22 18:30:09 +0100

    Changed the way to set width of comments

--- app/src/main/java/com/adamg/hnreader/adapter/CommentsAdapter.kt
+++ app/src/main/java/com/adamg/hnreader/adapter/CommentsAdapter.kt
@@ -1,12 +1,11 @@
 package com.adamg.hnreader.adapter
 
 import android.app.Activity
-import android.graphics.Point
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
+import android.widget.LinearLayout
 import com.adamg.hnreader.R
 import com.adamg.hnreader.models.Comment
 import kotlinx.android.synthetic.main.comment_card.view.*
@@ -14,7 +13,7 @@ import kotlinx.android.synthetic.main.comment_card.view.*
 
 class CommentsAdapter(var context: Activity, var comments: List<Comment>): RecyclerView.Adapter<CommentsAdapter.CommentViewHolder>() {
 
-    private var leftMargin = -1
+    private val cardViewMaxWeight = 20
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CommentViewHolder{
         val view = LayoutInflater.from(parent?.context).inflate(R.layout.comment_card, parent, false)
@@ -33,26 +32,9 @@ class CommentsAdapter(var context: Activity, var comments: List<Comment>): Recyc
             view.commentAuthor.text = comment.user
             view.commentAgo.text = comment.time_ago
             view.commentContent.text = comment.content
-            val layoutParams = view.cardView.layoutParams as FrameLayout.LayoutParams
-            if (leftMargin == -1) {
-                leftMargin = layoutParams.leftMargin
-            }
-            if (comment.level != 0) {
-                layoutParams.leftMargin = calculateWidth(comment.level)
-                view.cardView.layoutParams = layoutParams
-            } else {
-                layoutParams.leftMargin = leftMargin
-                view.cardView.layoutParams = layoutParams
-            }
-        }
-
-        private fun calculateWidth(level: Int): Int {
-            val display = context.windowManager.defaultDisplay
-            var point = Point()
-            display.getSize(point)
-            val width = point.x
-
-            return (width/20) * level + 4
+            val layoutParams = view.cardView.layoutParams as LinearLayout.LayoutParams
+            layoutParams.weight = (cardViewMaxWeight - comment.level).toFloat()
+            view.cardView.layoutParams = layoutParams
         }
     }
 }
