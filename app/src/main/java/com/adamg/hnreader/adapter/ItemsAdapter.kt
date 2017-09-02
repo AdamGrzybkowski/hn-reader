9268698f4a22223b0f1b4fc53ca360508fc4c603
Adam Grzybkowski <AdamGrzybkowski@users.noreply.github.com>
2017-09-02 18:56:26 +0200

    Feature/item card view (#7)
    * Deleted unused layouts
    
    * Moved app icon to mipmap folder
    
    * Changed item card layout

--- app/src/main/java/com/adamg/hnreader/adapter/ItemsAdapter.kt
+++ app/src/main/java/com/adamg/hnreader/adapter/ItemsAdapter.kt
@@ -1,18 +1,20 @@
 package com.adamg.hnreader.adapter
 
+import android.opengl.Visibility
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.adamg.hnreader.R
 import com.adamg.hnreader.models.Item
+import com.adamg.hnreader.models.ItemType
 import com.adamg.hnreader.views.listfragments.ItemListener
-import kotlinx.android.synthetic.main.story_card.view.*
+import kotlinx.android.synthetic.main.item_card.view.*
 
 class ItemsAdapter(var stories: List<Item>, val itemListener: ItemListener): RecyclerView.Adapter<ItemsAdapter.NewItemsViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): NewItemsViewHolder {
-        val view = LayoutInflater.from(parent?.context).inflate(R.layout.story_card, parent, false)
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_card, parent, false)
         return NewItemsViewHolder(view)
     }
 
@@ -25,11 +27,20 @@ class ItemsAdapter(var stories: List<Item>, val itemListener: ItemListener): Rec
 
     inner class NewItemsViewHolder(val view: View): RecyclerView.ViewHolder(view) {
         fun bindStory(item: Item){
-            view.storyTitle.text = item.title
-            view.storyBy.text = item.user
-            view.storyPoints.text = item.points.toString()
-            view.storyTime.text = item.time_ago
-            view.commentsCount.text = item.comments_count.toString()
+            view.itemTitle.text = item.title
+            item.user?.let { view.itemUser.text = view.context.getString(R.string.value_dot_separator, it) }
+            val points = item.points
+            view.itemPointsCount.text = points?.let { view.context.resources.getQuantityString(R.plurals.points, it, points) }
+            view.itemTimeAgo.text = item.time_ago
+            view.itemCommentsCount.text = view.context.resources.getQuantityString(R.plurals.comments, item.comments_count, item.comments_count)
+            view.itemDomain.text = item.domain
+            if (item.type == ItemType.JOB.value) {
+                view.itemPointsCount.visibility = View.GONE
+                view.itemCommentsCount.visibility = View.GONE
+            } else {
+                view.itemPointsCount.visibility = View.VISIBLE
+                view.itemCommentsCount.visibility = View.VISIBLE
+            }
             view.setOnClickListener{ itemListener.onItemClicked(item) }
         }
     }
---
+++ app/src/main/java/com/adamg/hnreader/models/ItemType.kt
@@ -0,0 +1,7 @@
+package com.adamg.hnreader.models
+
+enum class ItemType(val value: String) {
+    JOB("job"),
+    ASK("ask"),
+    LINK("link")
+}
